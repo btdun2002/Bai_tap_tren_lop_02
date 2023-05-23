c8a375ff963979f079a8dc1675ee8108123a19f9
root <root@Old-HP.localdomain>
2023-05-23 17:05:57 +0700

    First commit

--- udp_select.c
+++ udp_select.c
@@ -12,20 +12,38 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc < 4)
+    {
+        fprintf(stderr, "Usage: %s <server_ip> <sender_port> <receiver_port>\n", argv[0]);
+        return 1;
+    }
+
     int sender = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     int receiver = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
+    if (sender == -1 || receiver == -1)
+    {
+        perror("socket() failed");
+        return 1;
+    }
+
     struct sockaddr_in addr_s;
+    memset(&addr_s, 0, sizeof(addr_s));
     addr_s.sin_family = AF_INET;
     addr_s.sin_addr.s_addr = inet_addr(argv[1]);
     addr_s.sin_port = htons(atoi(argv[2]));
 
     struct sockaddr_in addr_r;
+    memset(&addr_r, 0, sizeof(addr_r));
     addr_r.sin_family = AF_INET;
-    addr_r.sin_addr.s_addr = inet_addr(INADDR_ANY);
+    addr_r.sin_addr.s_addr = htonl(INADDR_ANY);
     addr_r.sin_port = htons(atoi(argv[3]));
 
-    bind(receiver, (struct sockaddr *)&addr_r, sizeof(addr_r));
+    if (bind(receiver, (struct sockaddr *)&addr_r, sizeof(addr_r)) == -1)
+    {
+        perror("bind() failed");
+        return 1;
+    }
 
     fd_set fdread, fdtest;
     FD_ZERO(&fdread);
@@ -53,10 +71,17 @@ int main(int argc, char *argv[])
         if (FD_ISSET(receiver, &fdtest))
         {
             ret = recvfrom(receiver, buf, sizeof(buf), 0, NULL, NULL);
+            if (ret == -1)
+            {
+                perror("recvfrom() failed");
+                break;
+            }
             buf[ret] = 0;
-            printf("Received: %d", ret);
+            printf("Received: %s", buf);
         }
     }
     close(sender);
     close(receiver);
+
+    return 0;
 }
